29906415ab7beec1b060f7691ca1d7ede9d5b505
gzapataz <gab.zapata@uniandes.edu.co>
2016-09-19 16:38:38 -0500

    Cambios hechos desde Eclipse

--- PruebaGit/appClientModule/co/univ/prueba/HelloWorld.java
+++ PruebaGit/appClientModule/co/univ/prueba/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo Cambiado en GIT directamente");
+		System.out.println("Adicione este metodo a la funcionalidad");
+		
 
 	}
 
