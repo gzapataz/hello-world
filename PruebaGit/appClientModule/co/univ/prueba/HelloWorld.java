595f5f313d89a9425b82e731210203fe575b77b6
gzapataz <gab.zapata@uniandes.edu.co>
2016-09-19 14:42:40 -0500

    Subiendo

---
+++ PruebaGit/appClientModule/co/univ/prueba/HelloWorld.java
@@ -0,0 +1,11 @@
+package co.univ.prueba;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+
+	}
+
+}
